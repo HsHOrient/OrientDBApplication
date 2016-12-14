52a46555f7d6afd338c72517c2c049ea55f838ae
Jan Philipp Timme <jan.philipp@timme.it>
2016-12-14 16:27:27 +0100

    Clean up main.java

--- src/main/java/de/hsh/inform/orientdb_project/Main.java
+++ src/main/java/de/hsh/inform/orientdb_project/Main.java
@@ -1,7 +1,5 @@
 package de.hsh.inform.orientdb_project;
 
-import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
-
 import de.hsh.inform.orientdb_project.orientdb.OrientDbHelperService;
 import de.hsh.inform.orientdb_project.util.ConfigPropertiesReader;
 
@@ -13,40 +11,9 @@ public class Main {
 				config.dbpass);
 		System.out.println("Using database: " + odhs.getDbUri(true));
 
-		// Get "handle" for database to pass to import service
-		//OrientGraphNoTx ogf = odhs.getOrientGraphNoTx();
-		
 		CommandLineInterface cli = new CommandLineInterface(odhs);
 		cli.run();
-		
-		/*
-		TcpConnectionRepository tcr = new TcpConnectionRepository(ogf);
-		List<TcpConnectionModel> result = tcr.findByActiveWhen(901713642);
-		for(TcpConnectionModel m : result) {
-			System.out.println(m.toString());
-		}
-		
-		long r = tcr.getTotalDataVolumePerMinuteBetweenHosts("172.16.114.207", "206.251.19.72");
-		System.out.println("Bytes per Second: " + r);
 
-		HostRepository hr = new HostRepository(ogf);
-		for(HostModel hm : hr.findByConnectionsTo("197.218.177.69", 25)) {
-			System.out.println(hm);
-		}
-		
-		for(HostModel hm : hr.findAllByConnectionsToOutsideHosts()) {
-			System.out.println(hm);
-		}
-		
-		EthernetFrameRepository efr = new EthernetFrameRepository(odhs.getDatabaseDocument());
-		List<EthernetFrameModel> efrbyteResult = efr.findAllByRawData(new byte[] {
-			(byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF
-		});
-		for(EthernetFrameModel em : efrbyteResult) {
-			System.out.println(em);
-		}
-		*/
-		
 		// Done
 		odhs.close();
 	}
